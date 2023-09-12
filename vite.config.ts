25b36232f21dd82fa841fed811edc82ea229cfa4
ZkEdison <214236543@qq.com>
2023-09-12 17:34:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  base: '/end-time/'
+  base: '/end-time/',
   plugins: [
     reactRefresh(),
     legacy({
